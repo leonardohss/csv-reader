1da8280c5e500999e9fc7341fde52f5b38851e4a
Leonardo (NoteMauricy) <leorick97@gmail.com>
2019-08-03 10:41:24 -0300

    Create the project, the StreamReader function an the Product class

---
+++ Entities/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsvReader.Entities
+{
+    class Product
+    {
+        public string name { get; set; }
+        public double price { get; set; }
+        public int quantity { get; set; }
+
+        public Product() { }
+
+        public Product(string name, double price, int quantity)
+        {
+            this.name = name;
+            this.price = price;
+            this.quantity = quantity;
+        }
+
+        public double Total()
+        {
+            return price * quantity;
+        }
+
+        public override string ToString()
+        {
+            return name + "," + Total().ToString("F2"); 
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Globalization;
+using CsvReader.Entities;
+
+namespace CsvReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathSource = @"c:\temp\file1.csv";
+
+            Product[] products = new Product[10];
+            
+            try
+            {
+                using (StreamReader sr = File.OpenText(pathSource))
+                {
+                    int x = 0; 
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = (sr.ReadLine().Split(','));
+                        string name = line[0];
+                        double price = double.Parse(line[1], CultureInfo.InvariantCulture);
+                        int quantity = int.Parse(line[2]);
+                        products[x] = new Product(name, price, quantity);
+                        x++;
+                    }
+                }
+
+                foreach(Product product in products)
+                {
+                    Console.WriteLine(product);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred!");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
